0d007279fa68fde1cabb74e93a7bead1d545e2de
priyanshu sharma <132159136+priyanshu052@users.noreply.github.com>
2025-06-09 23:47:17 +0530

    removed alret

--- js/script.js
+++ js/script.js
@@ -21,10 +21,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 // Form submission
-document.querySelectorAll('form').forEach(form => {
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        alert('Thank you for your message! I will get back to you soon.');
-        this.reset();
-    });
-});
